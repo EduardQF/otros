95eaa02ce5a50784908d5ade40bd52625fbd42af
Eduardo Queupumil F <e.queupumil01@ufromail.cl>
2016-08-19 20:45:29 -0400

    juego de la vida
    proyect.2 game no life

---
+++ game_no_life/Game_no_life.java
@@ -0,0 +1,15 @@
+
+package game_no_life;
+
+/**
+ *
+ * @author Eduardo QF
+ */
+public class Game_no_life {
+
+    public static void main(String[] args) {
+        vista vista=new vista();
+        vista.setVisible(true);
+    }
+    
+}
